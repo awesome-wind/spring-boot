1788bf9ab5fa44bab390b50e48b806e486ffbe7e
wangzhang <wangzhang2016@gmail.com>
2017-12-13 20:46:59 +0800

    complete

--- springboot_updownload/src/main/java/com/springboot/file/controller/FileController.java
+++ springboot_updownload/src/main/java/com/springboot/file/controller/FileController.java
@@ -29,9 +29,13 @@ public class FileController {
         return "oneFile";
     }
 
+    @RequestMapping("/multiFiles")
+    public String multiFiles() {
+        return "multiFiles";
+    }
+
     @RequestMapping("/oneFileDo")
-    @ResponseBody
-    public String oneFileDo(@RequestParam("file")MultipartFile file) {
+    public String oneFileDo(@RequestParam("file") MultipartFile file) {
         /**
          * 获取到原始文件名，定义新的唯一的文件名，设置文件保存的位置
          */
@@ -44,15 +48,15 @@ public class FileController {
          * 将文件流以字节码流上传指定的位置
          */
         File f = new File(path);
-        if(!f.exists())
+        if (!f.exists())
             f.mkdir();
-        if(!file.isEmpty()) {
+        if (!file.isEmpty()) {
             try {
-                FileOutputStream fos = new FileOutputStream(path+newFileName);
+                FileOutputStream fos = new FileOutputStream(path + newFileName);
                 InputStream in = file.getInputStream();
 
                 int b = 0;
-                while ((b = in.read())!=-1) {
+                while ((b = in.read()) != -1) {
                     fos.write(b);
                 }
                 fos.close();
@@ -62,26 +66,72 @@ public class FileController {
             }
         }
 
-        System.out.println("上传图片到："+path+newFileName);
-        return "上传成功";
+        System.out.println("上传图片到：" + path + newFileName);
+        return "index";
+    }
+
+    @RequestMapping("/multiFilesDo")
+    public String multiFilesDo(@RequestParam("files")MultipartFile []files) {
+
+        String path = "F:\\IdeaProjects\\springboot_updownload\\src\\main\\resources\\static\\images\\";
+        File f = new File(path);
+        if(!f.exists())
+            f.mkdir();
+
+        for (int i=0;i<files.length;i++ ) {
+            String fileName = files[i].getOriginalFilename();
+            String newFileName = UUID.randomUUID() + "-"+fileName;
+
+            if(!files[i].isEmpty()) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(path+newFileName);
+                    InputStream in = files[i].getInputStream();
+
+                    int b=0;
+                    while((b=in.read())!=-1) {
+                        fos.write(b);
+                    }
+
+                    fos.close();
+                    in.close();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("上传图片到：" + path + newFileName);
+        }
+
+        return "index";
     }
 
     @RequestMapping("/showFiles")
     public String showFiles(ModelMap model) {
         String path = "F:\\IdeaProjects\\springboot_updownload\\src\\main\\resources\\static\\images\\";
-        Map<String,File> fileNameMap = new HashMap<>();
+        Map<String, String> fileNameMap = new HashMap<>();
 
         /**
-         * 获取到所有的文件，并将文件名与文件以K-V对存放在map中
+         * 获取到所有的文件，并将文件路径与文件真实名以K-V对存放在map中
          */
         File[] fileList = new File(path).listFiles();
-        for(File file:fileList) {
-            fileNameMap.put(file.getName(),file);
-            System.out.println(file.getName());
+        for (File file : fileList) {
+            String realName = getRealName(file.getName());
+            fileNameMap.put("/images/" + file.getName(), realName);
+//            System.out.println(file.getName()+" "+realName);
         }
 
-        model.addAttribute("fileNameMap",fileNameMap);
+        model.addAttribute("fileNameMap", fileNameMap);
         return "show";
     }
+
+    private String getRealName(String name) {
+        String[] str = name.split("-");
+
+        String realName = str[5];
+        for (int i = 6; i < str.length; i++)
+            realName += str[i];
+        return realName;
+    }
+
 
 }
